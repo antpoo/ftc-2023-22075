77a132d6dc5ec02743825436fb85b6fe206d2951
Antpoo <1ijangand2@hdsb.ca>
2023-09-21 16:58:34 -0400

    Started camera code finally!

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/util/Camera.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode.util;
+
+import org.openftc.easyopencv.OpenCvCamera;
+
+public class Camera {
+
+    OpenCvCamera camera;
+    AprilTagDetectionPipeline aprilTagDetectionPipeline;
+
+
+
+
+}
